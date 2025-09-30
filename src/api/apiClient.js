54a579e900d57e65f4ac1f97aa46e57c67bccf08
Sathish R <sathishravichandran9345@gmail.com>
2025-09-30 10:26:46 +0530

    url-frontend-to-backend

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL:'https://key-system-server-xqnd.onrender.com/api',
+  baseURL:'https://server.sbbs.co.in/key-systems/api',
   withCredentials: true,
 });
 
